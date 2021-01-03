0c2d3c5193f7fb86088b7e34cac098ef7e88496e
NaNostyle <larrieu.arnaud@gmail.com>
2021-01-03 22:48:26 +0100

    refactoring object

--- horoscope-generator.js
+++ horoscope-generator.js
@@ -19,50 +19,88 @@ const horoscope = {
 
   themes: ["Amour", "Quarantaine", "Travail", "Argent", "Santé"],
 
-  luckyAdjectives: ["beaucoup", "énormément", "incroyablement", "très"],
+  adjectives: {
+    lucky: {
+      luckyAdjectives: ["beaucoup", "énormément", "incroyablement", "très"],
+    },
+    unlucky: {
+      unluckyDayAdjectives: [
+        "catastrophique",
+        "très mauvaise",
+        "à vite oublier",
+      ],
+    },
+  },
 
-  unluckyAdjectives: ["très peu", "pas du tout", "peu"],
+  theme: {
+    love: {
+      lucky: {
+        actions: [
+          "sortir dans la rue, vous rencontrerez l'âme soeur à coup sur",
+          "vous jeter dans les bras du premier inconnu, c'est le bon",
+        ],
+      },
+      unlucky: {
+        actions: [
+          "surveiller votre mari si vous en avez un",
+          "laisser tomber si vous comptiez trouver le grand amour",
+        ],
+      },
+    },
+    quarantine: {
+      lucky: {
+        actions: [
+          "sortir de chez vous sans masque, vous n'attraperez pas le coronavirus",
+          "embrasser votre mari même si il a le coronavirus, vous ne l'attraperez pas",
+        ],
+      },
+      unlucky: {
+        actions: [
+          "rester chez vous sous peine d'attrapper le coronavirus",
+          "mettre 2 masques FFP2 au cas ou vous sortiriez de chez vous",
+        ],
+      },
+    },
+    work: {
+      lucky: {
+        actions: [
+          "postuler dans l'entreprise de vos rêves",
+          "postuler en tant que développeur Full Stack chez Apple",
+        ],
+      },
+      unlucky: {
+        actions: [
+          "vous inscrire chez Pôle Emploi",
+          "revoir à la baisse vos objectifs de carrière",
+        ],
+      },
+    },
 
-  luckyDayAdjectives: ["excellente", "merveilleuse", "inoubliable"],
+    money: {
+      lucky: {
+        actions: ["jouer au loto", "parier sur un cheval"],
+      },
+      unlucky: {
+        actions: ["vendre votre voiture", "vendre votre maison"],
+      },
+    },
 
-  unluckyDayAdjectives: ["catastrophique", "très mauvaise", "à vite oublier"],
-
-  luckyLoveAction: [
-    "sortir dans la rue, vous rencontrerez l'âme soeur à coup sur",
-    "vous jeter dans les bras du premier inconnu, c'est le bon",
-  ],
-  luckyQuarantineAction: [
-    "sortir de chez vous sans masque, vous n'attraperez pas le coronavirus",
-    "embrasser votre mari même si il a le coronavirus, vous ne l'attraperez pas",
-  ],
-  luckyworkAction: [
-    "postuler dans l'entreprise de vos rêves",
-    "postuler en tant que développeur Full Stack chez Apple",
-  ],
-  luckyMoneyAction: ["jouer au loto", "parier sur un cheval"],
-  luckyHealthAction: [
-    "vous levez si vous êtes hémiplégique",
-    "allumer la radio si vous êtes sourd",
-    "allumer la TV si vous êtes aveugle",
-  ],
-
-  unluckyLoveAction: [
-    "surveiller votre mari si vous en avez un",
-    "laisser tomber si vous comptiez trouver le grand amour",
-  ],
-  unluckyQuarantineAction: [
-    "rester chez vous sous peine d'attrapper le coronavirus",
-    "mettre 2 masques FFP2 au cas ou vous sortiriez de chez vous",
-  ],
-  unluckyworkAction: [
-    "vous inscrire chez Pôle Emploi",
-    "revoir à la baisse vos objectifs de carrière",
-  ],
-  unluckyMoneyAction: ["vendre votre voiture", "vendre votre maison"],
-  unluckyHealthAction: [
-    "vous acheter un fauteil roulant vous allez en avoir besoin",
-    "prendre rendez-vous chez le médecin",
-  ],
+    health: {
+      lucky: {
+        actions: [
+          "vous levez si vous êtes hémiplégique",
+          "allumer la radio si vous êtes sourd",
+          "allumer la TV si vous êtes aveugle",
+        ],
+      },
+      unlucky: {
+        actions: [
+          "vous acheter un fauteil roulant vous allez en avoir besoin",
+          "prendre rendez-vous chez le médecin",
+        ],
+      },
+    },
+  },
 
   luck() {
     const luck = [true, false];
@@ -71,30 +109,31 @@ const horoscope = {
 
   adjectives() {
     if (this.luck()) {
-      const chanceAdjective = this.luckyAdjectives[
+      this.chanceAdjective = this.luckyAdjectives[
         Math.floor(Math.random() * this.luckyAdjectives.length)
       ];
-      const dayAdjective = this.luckyAdjectives[
+      this.dayAdjective = this.luckyAdjectives[
         Math.floor(Math.random() * this.luckyDayAdjectives.length)
       ];
     } else {
-      const chanceAdjective = this.unluckyAdjectives[
+      this.chanceAdjective = this.unluckyAdjectives[
         Math.floor(Math.random() * this.unluckyAdjectives.length)
       ];
-      const dayAdjective = this.unLuckyDayAdjectives[
+      this.dayAdjective = this.unluckyDayAdjectives[
         Math.floor(Math.random() * this.unluckyAdjectives.length)
       ];
     }
   },
 
   randomSentence() {
+    this.adjectives();
     if (this.luck()) {
       console.log(
         `Vous êtes du signe ${
           this.zodiacSign[Math.floor(Math.random() * this.zodiacSign.length)]
         }. Sur le thème ${
           this.themes[Math.floor(Math.random() * this.themes.length)]
-        } et`
+        } et ${this.chanceAdjective}`
       );
     } else {
       console.log("unlucky");
